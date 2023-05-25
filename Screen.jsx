01c1bd32febe3de58e7d58f34cb80bfe012fa8ee
Bhushan Sharma <99554438+bhushansharma87@users.noreply.github.com>
2023-05-25 13:47:02 +0530

    Add files via upload

---
+++ Screen.jsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from 'react'
+
+const Screen = () => {
+    const[city,setCity]= useState('Delhi');
+
+    let myApi = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=143454aa39bbe3442a890cdbf3f9db36`
+
+    const[apiLink,setApi] = useState(myApi);
+
+    const[apiData,setapiData] = useState({
+        main:{
+            temp:0
+        }
+    });
+
+    useEffect(()=>{
+        const getApi = async()=>{
+            const response = await fetch(apiLink);
+            const result = await response.json();
+
+            setapiData(()=>({
+                ...result
+            }))
+            console.log(apiData);
+        }
+        getApi();
+    },[apiLink])
+
+    const handleCityInput=(e)=>{
+        setCity(e.target.value);
+        
+    }
+
+    const handleSearch=()=>{
+        setApi(myApi);
+    }
+
+    
+    return (
+
+    <div>
+        <h2>Weather Application</h2>
+        <br />
+        <input type="text" placeholder="Enter City Name" onChange={handleCityInput}/>
+        <button onClick={handleSearch}>Search</button>
+        <div>
+            <h2 style={{display:'inline'}}>Temperature : </h2>
+            <p style={{display:'inline'}}>{apiData.main.temp}</p>
+        </div>
+    </div>
+    )
+}
+
+export default Screen
